6179df4c59a9fce1332bf2dbcc2eb44376b685ae
sunibnesazzad <sunibne.sazzad@gmail.com>
2023-06-13 21:30:55 +0600

    Installing bootstrap and Completing the Navbar part

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 // import Home from './pages/Home.js';
 
 import MyRouter from './router/index.js';
-
+import Navbar from './components/Navbar.js';
 
 function App() {
   return (
     <div>
-      {/* <h1>Hello arpa</h1> */}
-      <Link to ="/"> Home </Link><br></br>
+      
+      {/* <Link to ="/"> Home </Link><br></br>
       <Link to ="/Contact-us"> Contact </Link><br></br>
-      <Link to ="/About-us"> About Us</Link>
+      <Link to ="/About-us"> About Us</Link> */}
 
+      <Navbar />
       <MyRouter />
 
       {/* <Home /> */}
