dc15463aff8a4ed85f91f57ad16a5a78fb41f5b9
sunibnesazzad <sunibne.sazzad@gmail.com>
2023-06-22 01:47:28 +0600

    Getting Api data from local host successfully and showing the loading part successfully

---
+++ src/components/Loading.js
@@ -0,0 +1,16 @@
+
+
+function Looading (){
+
+    return (
+        <div className="container mt-3">
+           <div className="spinner-border text-primary" role="status">
+               <span className="visually-hidden">Loading...</span>
+           </div>
+           <span>Loading .... wait</span>
+        </div>
+    );
+
+}
+
+export default Looading;
--- src/pages/Students.js
+++ src/pages/Students.js
@@ -1,17 +1,56 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import  Loading from "../components/Loading.js"
 
 
 function Student (){
 
+    //for showing Loading
+    const [loading, setloading] = useState(true);
+
+    //using useState for storing api data in a variable
+    const [students, setstudents] = useState([]);
+
     useEffect( () =>{
 
-        axios.get().then(res =>{
+        axios.get('http://localhost:8000/api/students').then(res => {
+             
+                  console.log(res);
+                  setstudents(res.data.students);
+                  //if data came from api loading will disappear
+                  setloading(false);
+
 
         });
 
     }, [] )
+
+    if(loading){
+        return(
+            <Loading />
+        )
+    }
+
+    var studentDetails = ""; 
+    studentDetails = students.map( (item, index) => {
+
+          return(
+            <tr key = {index} >
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.course}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+                <td>
+                    <Link to= "/" className="btn btn-success">Edit</Link>
+                </td>
+                <td>
+                    <button className="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+          )
+    });
 
     return (
         <div className="container mt-5">
@@ -36,6 +75,9 @@ function Student (){
                                         <th>Delete</th>
                                     </tr>
                                 </thead>
+                                <tbody>
+                                    {studentDetails}
+                                </tbody>
                             </table>
                         </div>
                     </div>
